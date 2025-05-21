7f98cda34dcb370796bbad2e025546eb00a8b934
senak <1418172577@qq.com>
2025-05-21 15:03:05 +0800

    1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) return 0;
+    node* slow, * fast;
+    slow = fast = head;
+    while (1) {
+        fast = fast->next;
+        if (fast == NULL) return 0;
+        fast = fast->next;
+        if (fast == NULL) return 0;
+        slow = slow->next;
+        if (fast == slow) {
+            return 1;
+        }
+    }
     return 0;
 }
